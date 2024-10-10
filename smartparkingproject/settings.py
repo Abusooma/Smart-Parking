bbc7d1aca216823fa8516b0044aa5eb0730018ef
Abusooma <soumahaboubacarsopra@gmail.com>
2024-10-10 19:47:12 +0100

    Update files settings.py and urls.py for static files

--- smartparkingproject/settings.py
+++ smartparkingproject/settings.py
@@ -140,9 +140,12 @@ LOGIN_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ajoutez ceci si vous avez des dossiers statiques supplémentaires
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
